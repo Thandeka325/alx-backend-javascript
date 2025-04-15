ecfd46b02cbafafa53dc70b4efd530b014b371a1
Thandeka325 <marhwayiza325@gmail.com>
2025-04-15 17:25:35 +0000

    Task 6: Handle multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,5 +5,11 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const signupPromise = signUpUser(firstName, lastName);
   const uploadPromise = uploadPhoto(fileName);
 
-  return Promise.allSettled([signupPromise, uploadPromise]);
+  return Promise.allSettled([signupPromise, uploadPromise]).then((results) => results.map((res) => {
+    if (res.status === 'fulfilled') {
+      return { status: res.status, value: res.value };
+    }
+
+    return { status: res.status, value: res.reason };
+  }));
 }
