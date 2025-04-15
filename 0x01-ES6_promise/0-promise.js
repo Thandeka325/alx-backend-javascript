530b287862d0671a3581697823b3203f60007f60
Thandeka325 <marhwayiza325@gmail.com>
2025-04-15 13:27:50 +0000

    Task 0: Keep every promise you make and only make promises you can keep

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,6 @@
+export default function getResponseFromAPI() {
+  return new Promise((resolve) => {
+    // Can either use resolve or reject here based on logic
+    resolve(); // For now, I chose to resolve it
+  });
+}
