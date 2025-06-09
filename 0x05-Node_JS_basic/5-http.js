b8cba37b3162527da1d5f7c5f3317ce607af4b01
Thandeka325 <marhwayiza325@gmail.com>
2025-06-09 17:31:45 +0000

    Task 5: Create a more complex HTTP server using Nodes HTTP module

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,62 @@
+const http = require('http');
+const fs = require('fs');
+
+const databaseFile = process.argv[2];
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf-8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+
+      const lines = data.split('\n').filter((line) => line.trim() !== '');
+      const students = lines.slice(1); // Skip header
+
+      const output = [];
+      output.push(`Number of students: ${students.length}`);
+
+      const fields = {};
+
+      students.forEach((line) => {
+        const [firstname, , , field] = line.split(',');
+        if (!fields[field]) {
+          fields[field] = [];
+        }
+        fields[field].push(firstname);
+      });
+
+      for (const [field, names] of Object.entries(fields)) {
+        output.push(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
+      }
+
+      resolve(output.join('\n'));
+    });
+  });
+}
+
+const app = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello ALX!');
+  } else if (req.url === '/students') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('This is the list of our students\n');
+
+    countStudents(databaseFile)
+      .then((output) => {
+        res.end(output);
+      })
+      .catch((err) => {
+        res.end(err.message);
+      });
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not found');
+  }
+});
+
+app.listen(1245);
+
+module.exports = app;
